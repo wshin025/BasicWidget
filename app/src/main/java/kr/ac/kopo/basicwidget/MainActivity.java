ea5cdcb5d46e2d0bf309f03cc316e3be5f14e591
wshin025 <wody0506@gmail.com>
2025-03-31 16:04:40 +0900

    TextView의 속성들을 JAVA에서 설정1

--- app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
@@ -2,13 +2,13 @@ package kr.ac.kopo.basicwidget;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.actvity_main2);
+
+
         TextView text1 = findViewById(R.id.text1);
         TextView text2 = findViewById(R.id.text2);
         TextView text3 = findViewById(R.id.text3);
@@ -32,7 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         text3.setTextSize(20);
         text3.setText("Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성");
-        text3.setSingleLine(true);
+        text3.setSingleLine(true);  // Sets TextView to single line
 
+
+        EditText edit1 = findViewById(R.id.edit1);
+        TextView textResult = findViewById(R.id.textResult);
+        Button btnResult = findViewById(R.id.btnResult);
+
+
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String univName = edit1.getText().toString();
+                String result = univName + "에 합격하신 것을 진심으로 축하드립니다.";
+                textResult.setText(result);
+                textResult.setTextColor(Color.GREEN);
+            }
+        });
     }
 }
